3dc3dca55ac7cf4d2fdf414822cd265c9945ba60
navabhi22 <navabhi22@gmail.com>
2022-02-25 14:10:56 +0530

    Abhinav Add Compute Emloyee Wages for Multiple Company

---
+++ ForMultipleCompanies.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class ForMultipleCompanies
+    {
+        public void WagesForMultipleCompanies(int NumberOfCompanies)
+        {
+            
+            for ( int i = 0; i < NumberOfCompanies; i++ )
+            {
+                Console.WriteLine("Welcome to Employee Wage Program for moltiple Companies");
+                Console.WriteLine("Enter the Name of company");
+                string company = Console.ReadLine();
+                Console.WriteLine("Enter the wage rate of {0} company", company);
+                int WagePerHour = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the number of working days per month in {0} company", company);
+                int MaxWorkingDays = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the number of maximum working hour per month in {0} company", company);
+                int MaxWorkingHour = Convert.ToInt32(Console.ReadLine());
+
+                MonthlyWagesForMultipleCompanies monthlyWage = new MonthlyWagesForMultipleCompanies();
+                monthlyWage.WageForMonth(WagePerHour, MaxWorkingDays, MaxWorkingHour);
+            }
+        }
+    }
+}

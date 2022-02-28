d5c69de2d0b55d60fd7c71739391321c61c70038
navabhi22 <navabhi22@gmail.com>
2022-02-28 16:02:12 +0530

    Abhinav Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -6,12 +6,10 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            EmployeeWageBuilderObject dMart = new EmployeeWageBuilderObject("dMart",30,25,120);
-            EmployeeWageBuilderObject Parle = new EmployeeWageBuilderObject("Parle", 30, 25, 120);
-            dMart.ComputeEmpWage();
-            Console.WriteLine(dMart.ToString());
-            Parle.ComputeEmpWage();
-            Console.WriteLine(Parle.ToString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("dMart", 30, 25, 120);
+            empWageBuilder.addCompanyEmpWage("Parle", 30, 25, 120);
+            empWageBuilder.computeEmpWage();
         }
             
     }
